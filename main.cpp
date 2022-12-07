6f99476a626e8ebd8291610c2c6d074d681f1b14
DmytroMoshchenko <118250660+DmytroMoshchenko@users.noreply.github.com>
2022-12-07 17:35:14 +0200

    ch
    ch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-// Задача 1.1 Варіант 13;
+// Task 1.1 Variant 13;
 #include <iostream>
 #include <iomanip>
 using namespace std;
 int main()
+{
+    double radius, area, circumference, diameter;
+    const double PI = 3.14159;
+    cout << "Enter the diameter of the circle\n";
+    cin >> diameter;
+    area = PI * diameter / 4;
+    cout << fixed << setprecision(1);
+    cout << "Тhe area of the circle is " << area << endl;
+    cout << "Enter the radius of the circle\n";
+    cin >> radius;
+    circumference = 2 * PI * radius;
+    cout << fixed << setprecision(1);
+    cout << "The length of the circle is " << circumference << endl;
+    system("pause");
+    return 0;
+}
 {
     double radius, area, circumference, diameter;
     const double PI = 3.14159;
